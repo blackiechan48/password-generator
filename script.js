b3d0c3dbc369c59c43ad9a15c09158dbc2f8a83d
Olumide Olayeni <oluyeni@gmail.com>
2024-01-29 15:22:27 +0000

    wrote code for all function

--- script.js
+++ script.js
@@ -98,17 +98,56 @@ var password ={
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
-
+  while(!password.length){
+    password.length =parseInt(prompt(`Please enter your required password length between 8 and 128`));
+    if (password.length < 8 || password.length > 128 || isNaN(password.length)) {
+      alert (`Enter a valid length for password`);
+      password.length = 0;
+      // this alert ensures only the right length of entries can be entered.
+    }
+   
+  }
+  password.numericCharacters =confirm(`click OK to include numerical characters`);
+password.specialCharacters =confirm(`click OK to include special characters`);
+password.lowerCasedCharacters =confirm(`click OK to include lowercase`);
+password.upperCasedCharacters =confirm(`xlick OK to include uppercase`)
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var random =Math.floor(Math.random() * arr.length);
+  var randomPassword = arr[random];
+  return randomPassword;
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  getPasswordOptions();
+  var result =[];
+  var Possiblecharacters =[];
+  var validCharacters =[];
+  if (password.specialCharacters){
+    Possiblecharacters =+ getRandom(specialCharacters)
+    validCharacters =+ getRandom(specialCharacters)
+  }
+  if (password.numericCharacters){
+    Possiblecharacters =+ getRandom(numericCharacters)
+    validCharacters =+ getRandom(numericCharacters)
+  }
+  if (password.lowerCasedCharacters){
+    Possiblecharacters =+ getRandom(lowerCasedCharacters)
+    validCharacters =+ getRandom(lowerCasedCharacters)
+  }
+  if(password.upperCasedCharacters){
+    Possiblecharacters =+ getRandom(upperCasedCharacters)
+    validCharacters =+ getRandom(upperCasedCharacters)
+  }
+  for (var i=0; i<password.length; i++){
+    var Possiblecharacters =getRandom(Possiblecharacters);
+    result.push(Possiblecharacters);
+    // Created if statement to check that the runs thrugh all user input options
+  }
 
 }
 
